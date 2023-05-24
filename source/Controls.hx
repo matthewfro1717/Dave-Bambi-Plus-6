df1d4ef69f7c56476b2d9c94324c5ed480f43915
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-05-24 19:57:27 -0400

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -12,8 +12,8 @@ import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
 
-#if (haxe >= "4.0.0")
-enum abstract Action(String) to String from String
+@:enum
+abstract Action(String) to String from String
 {
 	var UP = "up";
 	var LEFT = "left";
@@ -151,146 +151,6 @@ enum abstract Action(String) to String from String
 	var T10_R = "t10-release";
 	var T11_R = "t11-release";
 }
-#else
-@:enum
-abstract Action(String) to String from String
-{
-	var UP = "up";
-	var LEFT = "left";
-	var RIGHT = "right";
-	var DOWN = "down";
-	var UP_P = "up-press";
-	var LEFT_P = "left-press";
-	var RIGHT_P = "right-press";
-	var DOWN_P = "down-press";
-	var UP_R = "up-release";
-	var LEFT_R = "left-release";
-	var RIGHT_R = "right-release";
-	var DOWN_R = "down-release";
-	var ACCEPT = "accept";
-	var BACK = "back";
-	var PAUSE = "pause";
-	var RESET = "reset";
-	var CHEAT = "cheat";
-	var CENTER = "center";
-	var CENTER_P = "center-press";
-	var CENTER_R = "center-release";
-	
-	var L1 = "l1";
-	var U1 = "u";
-	var R1 = "r1";
-	var L2 = "l2";
-	var D1 = "d";
-	var R2 = "r2";
-
-	var L1_P = "l1-press";
-	var U1_P = "u-press";
-	var R1_P = "r1-press";
-	var L2_P = "l2-press";
-	var D1_P = "d-press";
-	var R2_P = "r2-press";
-
-	var L1_R = "l1-release";
-	var U1_R = "u-release";
-	var R1_R = "r1-release";
-	var L2_R = "l2-release";
-	var D1_R = "d-release";
-	var R2_R = "r2-release";
-
-	var A0 = "a0";
-	var A1 = "a1";
-	var A2 = "a2";
-	var A3 = "a3";
-	var A4 = "a4";
-	var A5 = "a5";
-	var A6 = "a6";
-
-	var A0_P = "a0-press";
-	var A1_P = "a1-press";
-	var A2_P = "a2-press";
-	var A3_P = "a3-press";
-	var A4_P = "a4-press";
-	var A5_P = "a5-press";
-	var A6_P = "a6-press";
-
-	var A0_R = "a0-release";
-	var A1_R = "a1-release";
-	var A2_R = "a2-release";
-	var A3_R = "a3-release";
-	var A4_R = "a4-release";
-	var A5_R = "a5-release";
-	var A6_R = "a6-release";
-
-	var N0 = "n0";
-	var N1 = "n1";
-	var N2 = "n2";
-	var N3 = "n3";
-	var N4 = "n4";
-	var N5 = "n5";
-	var N6 = "n6";
-	var N7 = "n7";
-	var N8 = "n8";
-
-	var N0_P = "n0-press";
-	var N1_P = "n1-press";
-	var N2_P = "n2-press";
-	var N3_P = "n3-press";
-	var N4_P = "n4-press";
-	var N5_P = "n5-press";
-	var N6_P = "n6-press";
-	var N7_P = "n7-press";
-	var N8_P = "n8-press";
-
-	var N0_R = "n0-release";
-	var N1_R = "n1-release";
-	var N2_R = "n2-release";
-	var N3_R = "n3-release";
-	var N4_R = "n4-release";
-	var N5_R = "n5-release";
-	var N6_R = "n6-release";
-	var N7_R = "n7-release";
-	var N8_R = "n8-release";
-
-	var T0 = "t0";
-	var T1 = "t1";
-	var T2 = "t2";
-	var T3 = "t3";
-	var T4 = "t4";
-	var T5 = "t5";
-	var T6 = "t6";
-	var T7 = "t7";
-	var T8 = "t8";
-	var T9 = "t9";
-	var T10 = "t10";
-	var T11 = "t11";
-
-	var T0_P = "t0-press";
-	var T1_P = "t1-press";
-	var T2_P = "t2-press";
-	var T3_P = "t3-press";
-	var T4_P = "t4-press";
-	var T5_P = "t5-press";
-	var T6_P = "t6-press";
-	var T7_P = "t7-press";
-	var T8_P = "t8-press";
-	var T9_P = "t9-press";
-	var T10_P = "t10-press";
-	var T11_P = "t11-press";
-
-	var T0_R = "t0-release";
-	var T1_R = "t1-release";
-	var T2_R = "t2-release";
-	var T3_R = "t3-release";
-	var T4_R = "t4-release";
-	var T5_R = "t5-release";
-	var T6_R = "t6-release";
-	var T7_R = "t7-release";
-	var T8_R = "t8-release";
-	var T9_R = "t9-release";
-	var T10_R = "t10-release";
-	var T11_R = "t11-release";
-}
-#end
 
 enum Device
 {
@@ -508,11 +368,7 @@ class Controls extends FlxActionSet
 	var _t10R = new FlxActionDigital(Action.T10_R);
 	var _t11R = new FlxActionDigital(Action.T11_R);
 
-	#if (haxe >= "4.0.0")
-	var byName:Map<String, FlxActionDigital> = [];
-	#else
 	var byName:Map<String, FlxActionDigital> = new Map<String, FlxActionDigital>();
-	#end
 
 	public var gamepadsAdded:Array<Int> = [];
 	public var keyboardScheme = KeyboardScheme.None;
@@ -900,153 +756,6 @@ class Controls extends FlxActionSet
 	inline function get_T10_R() return _t10R.check();
 	inline function get_T11_R() return _t11R.check();
 
-	#if (haxe >= "4.0.0")
-	public function new(name, scheme = None)
-	{
-		super(name);
-
-		add(_up);
-		add(_left);
-		add(_right);
-		add(_down);
-		add(_upP);
-		add(_leftP);
-		add(_rightP);
-		add(_downP);
-		add(_upR);
-		add(_leftR);
-		add(_rightR);
-		add(_downR);
-		add(_accept);
-		add(_back);
-		add(_pause);
-		add(_reset);
-		add(_cheat);
-		add(_key5);
-		add(_center);
-		add(_centerP);
-		add(_centerR);
-
-		add(_l1);
-		add(_l1P);
-		add(_l1R);
-		add(_d);
-		add(_dP);
-		add(_dR);
-		add(_r1);
-		add(_r1P);
-		add(_r1R);
-
-		add(_l2);
-		add(_l2P);
-		add(_l2R);
-		add(_u);
-		add(_uP);
-		add(_uR);
-		add(_r2);
-		add(_r2P);
-		add(_r2R);
-
-
-		add(_a0);
-		add(_a1);
-		add(_a2);
-		add(_a3);
-		add(_a4);
-		add(_a5);
-		add(_a6);
-
-		add(_a0P);
-		add(_a1P);
-		add(_a2P);
-		add(_a3P);
-		add(_a4P);
-		add(_a5P);
-		add(_a6P);
-
-		add(_a0R);
-		add(_a1R);
-		add(_a2R);
-		add(_a3R);
-		add(_a4R);
-		add(_a5R);
-		add(_a6R);
-
-		add(_n0);
-		add(_n1);
-		add(_n2);
-		add(_n3);
-		add(_n4);
-		add(_n5);
-		add(_n6);
-		add(_n7);
-		add(_n8);
-
-		add(_n0P);
-		add(_n1P);
-		add(_n2P);
-		add(_n3P);
-		add(_n4P);
-		add(_n5P);
-		add(_n6P);
-		add(_n7P);
-		add(_n8P);
-
-		add(_n0R);
-		add(_n1R);
-		add(_n2R);
-		add(_n3R);
-		add(_n4R);
-		add(_n5R);
-		add(_n6R);
-		add(_n7R);
-		add(_n8R);
-
-		add(_t0);
-		add(_t1);
-		add(_t2);
-		add(_t3);
-		add(_t4);
-		add(_t5);
-		add(_t6);
-		add(_t7);
-		add(_t8);
-		add(_t9);
-		add(_t10);
-		add(_t11);
-
-		add(_t0P);
-		add(_t1P);
-		add(_t2P);
-		add(_t3P);
-		add(_t4P);
-		add(_t5P);
-		add(_t6P);
-		add(_t7P);
-		add(_t8P);
-		add(_t9P);
-		add(_t10P);
-		add(_t11P);
-
-		add(_t0R);
-		add(_t1R);
-		add(_t2R);
-		add(_t3R);
-		add(_t4R);
-		add(_t5R);
-		add(_t6R);
-		add(_t7R);
-		add(_t8R);
-		add(_t9R);
-		add(_t10R);
-		add(_t11R);
-
-		for (action in digitalActions)
-			byName[action.name] = action;
-
-		setKeyboardScheme(scheme, false);
-	}
-	#else
 	public function new(name, scheme:KeyboardScheme = null)
 	{
 		super(name);
@@ -1193,7 +902,6 @@ class Controls extends FlxActionSet
 			scheme = None;
 		setKeyboardScheme(scheme, false);
 	}
-	#end
 
 	override function update()
 	{
@@ -1495,16 +1203,6 @@ class Controls extends FlxActionSet
 
 	public function copyFrom(controls:Controls, ?device:Device)
 	{
-		#if (haxe >= "4.0.0")
-		for (name => action in controls.byName)
-		{
-			for (input in action.inputs)
-			{
-				if (device == null || isDevice(input, device))
-					byName[name].add(cast input);
-			}
-		}
-		#else
 		for (name in controls.byName.keys())
 		{
 			var action = controls.byName[name];
@@ -1514,21 +1212,14 @@ class Controls extends FlxActionSet
 				byName[name].add(cast input);
 			}
 		}
-		#end
 
 		switch (device)
 		{
 			case null:
 				// add all
-				#if (haxe >= "4.0.0")
-				for (gamepad in controls.gamepadsAdded)
-					if (!gamepadsAdded.contains(gamepad))
-						gamepadsAdded.push(gamepad);
-				#else
 				for (gamepad in controls.gamepadsAdded)
 					if (gamepadsAdded.indexOf(gamepad) == -1)
 					  gamepadsAdded.push(gamepad);
-				#end
 
 				mergeKeyboardScheme(controls.keyboardScheme);
 
@@ -1569,11 +1260,7 @@ class Controls extends FlxActionSet
 			if(i == NONE) copyKeys.remove(i);
 		}
 
-		#if (haxe >= "4.0.0")
-		inline forEachBound(control, (action, state) -> addKeys(action, keys, state));
-		#else
 		forEachBound(control, function(action, state) addKeys(action, keys, state));
-		#end
 	}
 
 	/**
@@ -1587,11 +1274,7 @@ class Controls extends FlxActionSet
 			if(i == NONE) copyKeys.remove(i);
 		}
 
-		#if (haxe >= "4.0.0")
-		inline forEachBound(control, (action, _) -> removeKeys(action, keys));
-		#else
 		forEachBound(control, function(action, _) removeKeys(action, keys));
-		#end
 	}
 
 	inline static function addKeys(action:FlxActionDigital, keys:Array<FlxKey>, state:FlxInputState)
@@ -1971,27 +1654,17 @@ class Controls extends FlxActionSet
 	public function addGamepad(id:Int, ?buttonMap:Map<Control, Array<FlxGamepadInputID>>):Void
 	{
 		gamepadsAdded.push(id);
-		
-		#if (haxe >= "4.0.0")
-		for (control => buttons in buttonMap)
-			inline bindButtons(control, id, buttons);
-		#else
+
 		for (control in buttonMap.keys())
 			bindButtons(control, id, buttonMap[control]);
-		#end
 	}
 
 	inline function addGamepadLiteral(id:Int, ?buttonMap:Map<Control, Array<FlxGamepadInputID>>):Void
 	{
 		gamepadsAdded.push(id);
 
-		#if (haxe >= "4.0.0")
-		for (control => buttons in buttonMap)
-			inline bindButtons(control, id, buttons);
-		#else
 		for (control in buttonMap.keys())
 			bindButtons(control, id, buttonMap[control]);
-		#end
 	}
 
 	public function removeGamepad(deviceID:Int = FlxInputDeviceID.ALL):Void
@@ -2048,11 +1721,7 @@ class Controls extends FlxActionSet
 	 */
 	public function bindButtons(control:Control, id, buttons)
 	{
-		#if (haxe >= "4.0.0")
-		inline forEachBound(control, (action, state) -> addButtons(action, buttons, state, id));
-		#else
 		forEachBound(control, function(action, state) addButtons(action, buttons, state, id));
-		#end
 	}
 
 	/**
@@ -2061,11 +1730,7 @@ class Controls extends FlxActionSet
 	 */
 	public function unbindButtons(control:Control, gamepadID:Int, buttons)
 	{
-		#if (haxe >= "4.0.0")
-		inline forEachBound(control, (action, _) -> removeButtons(action, gamepadID, buttons));
-		#else
 		forEachBound(control, function(action, _) removeButtons(action, gamepadID, buttons));
-		#end
 	}
 
 	inline static function addButtons(action:FlxActionDigital, buttons:Array<FlxGamepadInputID>, state, id)
