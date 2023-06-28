a29e9f31f4a088f8b9ee8e4a90d13b397e656c2d
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-06-28 15:22:02 -0400

    Update Note.hx

--- source/Note.hx
+++ source/Note.hx
@@ -334,7 +334,7 @@ class Note extends FlxSprite
 			{
 				str = 'cheating';
 				str = 'rigged';
-				str = 'cheating-random'
+				str = 'cheating-random';
 			}
 		}
 		if (str == 'cheating' || str == 'rigged' || str = 'cheating-random' && PlayState.modchartoption) {
